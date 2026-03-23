8bba0ddb46e0f3ad7fbc5462fec00bd6fc788b8e
nemalikantikarthik <143393787+nemalikantikarthik@users.noreply.github.com>
2026-03-23 23:08:52 +0530

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PORT = os.getenv('PORT',8000)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
